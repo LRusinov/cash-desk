94184dbdce3d72a74fd16d5d4075fc02309227cb
lrusinov <lyubomir2009@abv.bg>
2025-04-22 17:21:22 +0300

    Initial commit. App creation.

---
+++ src/test/java/org/myapp/cashdesk/CashDeskApplicationTests.java
@@ -0,0 +1,13 @@
+package org.myapp.cashdesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashDeskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

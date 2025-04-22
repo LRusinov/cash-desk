4e93008265334656ee872053fff8e34c2dc62c31
lrusinov <lyubomir2009@abv.bg>
2025-04-22 17:25:05 +0300

    Removed redundant empty line.

--- src/main/java/org/myapp/cashdesk/CashDeskApplication.java
+++ src/main/java/org/myapp/cashdesk/CashDeskApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CashDeskApplication {
 
     public static void main(String[] args) {
-
         SpringApplication.run(CashDeskApplication.class, args);
     }
 

a47cfc20e0c36656d13f7729621271af5920a3f6
lrusinov <lyubomir2009@abv.bg>
2025-04-24 18:23:05 +0300

    Added spring security config

---
+++ src/main/java/org/myapp/cashdesk/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package org.myapp.cashdesk.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig{
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+       return http
+                .authorizeHttpRequests(auth->auth.anyRequest().permitAll())
+                .csrf(AbstractHttpConfigurer::disable)
+                .build();
+    }
+}
+

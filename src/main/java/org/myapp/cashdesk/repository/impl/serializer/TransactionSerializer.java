2515591d3d6dd6923d5739172902db01a02ce670
lrusinov <lyubomir2009@abv.bg>
2025-04-25 04:10:23 +0300

    Extracted magical numbers to private constants

--- src/main/java/org/myapp/cashdesk/repository/impl/serializer/TransactionSerializer.java
+++ src/main/java/org/myapp/cashdesk/repository/impl/serializer/TransactionSerializer.java
@@ -1,4 +1,4 @@
-package org.myapp.cashdesk.repository.serializer;
+package org.myapp.cashdesk.repository.impl.serializer;
 
 import org.myapp.cashdesk.exception.CashDeskParseException;
 import org.myapp.cashdesk.exception.CashDeskSerializationException;
@@ -10,8 +10,16 @@ import java.math.BigDecimal;
 import java.time.Instant;
 
 public class TransactionSerializer extends BaseFileSerializer<Transaction> {
-
     private static final int NUMBER_OF_TRANSACTION_FIELDS = 8;
+    private static final int TRANSACTION_CURRENCY_INDEX = 4;
+    private static final int TRANSACTION_ID_INDEX = 0;
+    private static final int CASHIER_ID_INDEX = 1;
+    private static final int CASHIER_NAME_INDEX = 2;
+    private static final int OPERATION_TYPE_INDEX = 3;
+    private static final int TRANSACTION_AMOUNT_INDEX = 5;
+    private static final int TRANSACTION_DENOMINATION_INDEX = 6;
+    private static final int NEW_CASHIER_BALANCE_INDEX = 7;
+    private static final int TRANSACTION_TIMESTAMP_INDEX = 8;
 
     @Override
     public String serialize(final Transaction transaction) {
@@ -40,20 +48,20 @@ public class TransactionSerializer extends BaseFileSerializer<Transaction> {
         validateCorrectNumberOfFields(NUMBER_OF_TRANSACTION_FIELDS, parts.length, line);
 
         try {
-            Currency currency = Currency.valueOf(parts[4]);
+            Currency currency = Currency.valueOf(parts[TRANSACTION_CURRENCY_INDEX]);
             return new Transaction(
-                    parts[0],
-                    Long.parseLong(parts[1]),
-                    parts[2],
-                    OperationType.valueOf(parts[3]),
+                    parts[TRANSACTION_ID_INDEX],
+                    Long.parseLong(parts[CASHIER_ID_INDEX]),
+                    parts[CASHIER_NAME_INDEX],
+                    OperationType.valueOf(parts[OPERATION_TYPE_INDEX]),
                     currency,
-                    new BigDecimal(parts[5]),
-                    parseDenominations(parts[6], currency),
-                    parseBalances(parts[7]),
-                    Instant.parse(parts[8])
+                    new BigDecimal(parts[TRANSACTION_AMOUNT_INDEX]),
+                    parseDenominations(parts[TRANSACTION_DENOMINATION_INDEX], currency),
+                    parseBalances(parts[NEW_CASHIER_BALANCE_INDEX]),
+                    Instant.parse(parts[TRANSACTION_TIMESTAMP_INDEX])
             );
         } catch (Exception e) {
-            throw new CashDeskParseException("Failed to transaction from line: " + line, e);
+            throw new CashDeskParseException("Failed to parse transaction from line: " + line, e);
         }
     }
 }
